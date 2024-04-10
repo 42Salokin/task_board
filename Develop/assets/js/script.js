b1298e98b8269e5627e782455c9d56ae5f228b85
Salokin <nicholas.dpoul@gmail.com>
2024-04-10 19:52:20 -0400

    added event listener for modal button, adds input items to local storage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,10 +1,14 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const taskBtn = document.querySelector('#taskBtn');
+const title = document.querySelector('#taskTitle');
+const dueDate = document.querySelector('#datepicker');
+const description = document.querySelector('#description');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    
 }
 
 // Todo: create a function to create a task card
@@ -35,4 +39,15 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     $( "#datepicker" ).datepicker();
+    taskBtn.addEventListener('click', function (event) {
+        const taskEntry = {
+            title: title.value,
+            dueDate: dueDate.value,
+            description: description.value,
+        }
+        taskList.push(taskEntry);
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+        console.log(taskEntry);
+        console.log(taskList);
+    });
 });
