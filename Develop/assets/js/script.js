a892e82a04e3b57b52cd3388b01f509a8babab2d
Salokin <nicholas.dpoul@gmail.com>
2024-04-11 09:53:35 -0400

    switched all commands over from vanilla js to jquery

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,10 +1,10 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const taskBtn = document.querySelector('#taskBtn');
-const title = document.querySelector('#taskTitle');
-const dueDate = document.querySelector('#datepicker');
-const description = document.querySelector('#description');
+const taskBtn = $('#taskBtn');
+const title = $("#taskTitle");
+const dueDate = $("#datepicker");
+const description = $("#description");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -13,7 +13,8 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const todoCards = document.querySelector('#todo-cards');
+    const todoCards = $("#todo-cards");
+
     let card = $("<div>");
     let cardHead = $('<div>');
     let cardBody = $("<div>");
@@ -22,12 +23,13 @@ function createTaskCard(task) {
     let cardBtn = $("<button>");
 
     card.attr('class', 'card');
+    card.attr('id', 'draggable');
     cardHead.attr('class', 'card-header');
     cardBody.attr('class', 'card-body');
     cardTitle.attr('class', 'card-title');
     cardText.attr('class', 'card-text');
     cardBtn.attr('class', 'btn btn-danger');
-// jQuery commands need the $() at the beginning
+
     cardHead.text('Task Title');
     cardTitle.text('Description');
     cardText.text('Due Date');
@@ -39,13 +41,14 @@ function createTaskCard(task) {
     card.append(cardHead);
     card.append(cardBody);
     todoCards.append(card);
-    console.log(card);
 
+    console.log(card);
+    renderTaskList();
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    $( "#draggable" ).draggable();
 }
 
 // Todo: create a function to handle adding a new task
@@ -66,7 +69,7 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     $( "#datepicker" ).datepicker();
-    taskBtn.addEventListener('click', function (event) {
+    taskBtn.on('click', function (event) {
         const taskEntry = {
             title: title.value,
             dueDate: dueDate.value,
