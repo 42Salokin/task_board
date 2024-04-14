a1d8162a3ce0e73fb3498f62763884c8cf6e9c41
Salokin <nicholas.dpoul@gmail.com>
2024-04-14 18:04:23 -0400

    added comments and filled out the README

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+// connect to the main screen button, modal inputs, and to-do column to attach the task cards
 const taskBtn = $('#taskBtn');
 const title = $("#taskTitle");
 const dueDate = $("#datepicker");
@@ -9,6 +10,7 @@ const todoCards = $("#todo-cards");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    // if nextID is nothing, it becomes the number 1, then increases by 1 each time this runs
     if (nextId === null) {
         nextId = 1;
     } else {
@@ -20,37 +22,43 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard() {
+    // pulls input items from storage and puts them into an array
     let lastTask = JSON.parse(localStorage.getItem("tasks")) || [];
-
+    // for each object in the array, create a card and add the input items to it as text
     for (let i=0; i < lastTask.length; i++) {
       let card = $("<div>").addClass('task-card draggable my-3');
       let cardHead = $('<div>').addClass('card-header').text(lastTask[i].title);
       let cardBody = $("<div>").addClass('card-body');
       let cardTitle = $("<h5>").addClass('card-title').text(lastTask[i].description);
       let cardText = $("<p>").addClass('card-text').text(lastTask[i].dueDate);
-      let cardBtn = $("<button>").addClass('btn btn-danger delete').text('Delete');
+    //   set the card button to delete the card on click
+      let cardBtn = $("<button>").addClass('btn btn-danger delete').text('Delete').on('click', handleDeleteTask);
 
-      card.attr('data-project-id');
-
+      card.attr('data-task-id', lastTask[i].id);
+    //   adds the card to the page
       cardBody.append(cardTitle, cardText, cardBtn);
       card.append(cardHead, cardBody);
-
-      todoCards.append(card);
-
+    //   this sets the status of the card so that it can be registered to a different column
+      if (lastTask[i].status === 'to-do') {
+            $('#todo-cards').append(card);
+      } else if (lastTask[i].status === 'in-progress') {
+            $('#in-progress').append(card);
+      } else {
+            $('#done').append(card)
+      }
+    //   compares today's date with the due date on the card
       let now = dayjs();
-      let taskDueDate = dayjs(lastTask[i].dueDate, 'DD/MM/YYYY');
-      console.log(now);
-      console.log(taskDueDate);
-
+      let taskDueDate = dayjs(lastTask[i].dueDate, 'MM/DD/YYYY');
+    //   and sets the color to yellow if before or on that day, red if after
       if (now.isBefore(taskDueDate, 'day')) {
         card.addClass('bg-warning text-white');
-      } else if (now.isAfter(taskDueDate)) {
+      } else if (now.isAfter(taskDueDate, 'day')) {
         card.addClass('bg-danger text-white');
         cardBtn.addClass('border-light');
+      } else if (now.isSame(taskDueDate, 'day')) {
+        card.addClass('bg-warning text-white');
       }
-
-      renderTaskList();
-      
+    //   clears the modal input fields so they're empty for the next task
       title.val('');
       description.val('');
       dueDate.val('');
@@ -59,27 +67,34 @@ function createTaskCard() {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    createTaskCard();
     $( ".draggable" ).draggable();
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+// I just put all this in the createTaskCard function, couldn't figure out why it needed a separate function
 }
 
 // Todo: create a function to handle deleting a task
+// I tried two different methods, a filter and a for/each, and neither works. Not sure what I need to change to make it work
 function handleDeleteTask(event){
-  cardBtn.on('click', function (event) {
     const taskID = $(this).attr('data-task-id');
-    taskList = taskList.filter(task => task.id !== parseInt(taskID));
+    console.log(taskID);
+    // taskList = taskList.filter(task => task.id !== parseInt(taskID));
+    taskList.forEach((task) => {
+        if (task.id === taskID) {
+            taskList.splice(taskList.indexOf(task), 1);
+        }
+    });
+    // taskID.remove();
     localStorage.setItem('tasklist', JSON.stringify(taskList));
     renderTaskList();
-  })
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+// This has something to do with the status change I set in lines 42-47, need to figure out what to do with this
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -87,7 +102,7 @@ $(document).ready(function () {
     $( "#datepicker" ).datepicker();
     $( ".droppable" ).droppable();
     taskBtn.on('click', function (event) {
-        // possibly put all this in handleAddTask
+        // puts the input values from storage into an object, along with the unique card id and a starter status
         const taskEntry = {
             id: generateTaskId(),
             title: title.val(),
@@ -99,6 +114,7 @@ $(document).ready(function () {
         localStorage.setItem('tasks', JSON.stringify(taskList));
         console.log(taskEntry);
         console.log(taskList);
-        createTaskCard();
+        renderTaskList();
     });
+    renderTaskList();
 });
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# task_board
-A task board application that allows a team to manage project tasks
+# Task Board
+
+## Description
+
+This is a taskboard application that allows inputting and organizing tasks. Using jQuery and Bootstrap, I took pre-written starter code and added functionality. When the 'Add Task' button is clicked, a modal pops up to allow a task title, description, and due to date to be put in. That input then displays as a card in the 'to do' column. The color of the card changes depending on whether the task due date is coming up or overdue. The cards are able to be dragged and placed in the other columns. 
+
+I will be adding functionality to make it so that the tasks can be deleted, and stay in their previously assigned columns on resetting the page. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Access updated website online at:
+
+https://42salokin.github.io/task_board/
+
+![Alt text](./Develop/assets/Screenshot.jpg)
+
+## Credits
+
+I frequently used advice from other developers in my class and Stack Overflow, and the information at W3schools to assist me with putting functions, methods, and properties in proper working order.
+
+I used jQuery and Bootstrap for much of the style and format
+
+## License
+
+MIT License
+
+Copyright (c) 2024 42Salokin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
